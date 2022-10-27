7c7bce8610aefb54ebcba700679e67917a8b44fd
rmslm <salem.rami.91@gmail.com>
2022-10-27 10:16:50 +0000

    modify notebook

--- load_matlab.py
+++ load_matlab.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 sim_excluded_idx = [121, 128, 137]
 
-data = sp.loadmat('dataset_battery_power/dataset1.mat')
+data = sp.loadmat('dataset_battery_voltage/dataset1.mat')
 columns=['time', 'courant', 'soc', 'temperature', 'voltage']
 
 data_total_rc1 = pd.DataFrame(data['RC1Model'][0][0][0], columns=columns)
@@ -28,8 +28,6 @@ for sim in range(1, len(data['RC1Model'][0][0])):
     data_total_rc1 = data_total_rc1.append(pd.DataFrame(data['RC1Model'][0][0][sim], columns=columns))
     data_total_rc2 = data_total_rc2.append(pd.DataFrame(data['RC2Model'][0][0][sim], columns=columns))
 
-    if sim == 20:
-        break
 data_total_rc1.reset_index(drop=True, inplace=True)
 data_total_rc2.reset_index(drop=True, inplace=True)
 
@@ -42,7 +40,7 @@ print(data_total.shape)
 data_total['delta_volt_rc2_rc1'] = data_total['voltage_rc2'] - data_total['voltage_rc1']
 data_total = data_total.drop(columns=['time_rc2', 'courant_rc2', 'soc_rc2'])
 
-data_total.to_csv('./dataset_battery_power/subset_test.csv', index=False)
+data_total.to_csv('./dataset_battery_voltage/subset_test.csv', index=False)
 
 
 
